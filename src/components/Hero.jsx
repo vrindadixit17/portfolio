8f6f0a51ebf4909490ccba5c49d06b6c18e81d2c
Vrinda Dixit <dixitvrinda1704@gmail.com>
2025-11-07 20:04:02 +0530

    lorem

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
 
         {/* Right text */}
         <p className="max-w-xs text-[#5862E9] text-center md:text-left text-sm md:text-base font-[Hanken_Grotesk] leading-relaxed">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
 
