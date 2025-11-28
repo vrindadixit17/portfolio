3ada79a372fbc0833b0cb0f74cd76fc9745a1915
Vrinda Dixit <dixitvrinda1704@gmail.com>
2025-11-28 20:52:20 +0530

    home pics 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 // src/components/Home.jsx
 import React from "react";
 
-// Import images from assets
+// Import your phone screenshots
 import img1 from "../assets/Screenshot 2025-11-20 192853.png";
 import img2 from "../assets/Screenshot 2025-11-20 192903.png";
 import img3 from "../assets/Screenshot 2025-11-20 192912.png";
@@ -14,18 +14,13 @@ export default function Home() {
   return (
     <section id="home" className="py-10">
       <div className="flex justify-center gap-8 overflow-x-auto px-4">
-        {images.map((img, i) => (
-          <div
-            key={i}
-            className="device-frame w-[180px] md:w-[220px] flex-shrink-0 bg-black"
-          >
-            <div className="h-[360px] md:h-[400px] bg-white rounded-xl overflow-hidden">
-              <img
-                src={img}
-                alt={`screen-${i}`}
-                className="object-cover w-full h-full"
-              />
-            </div>
+        {images.map((img, index) => (
+          <div key={index} className="flex-shrink-0">
+            <img
+              src={img}
+              alt={`phone-${index}`}
+              className="w-[180px] md:w-[220px] object-contain"
+            />
           </div>
         ))}
       </div>
